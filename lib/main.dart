a848160583b72aa5a26f64504d6550d1d0468ccc
Hemanth2001 <hhemanth660@gmail.com>
2022-11-02 01:57:44 -0400

    learn

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(Myapp());
+
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
