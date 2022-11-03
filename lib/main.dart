6afa82f4bb0fae6c623c0d22cc5333af35c12edb
Hemanth2001 <hhemanth660@gmail.com>
2022-11-03 06:24:07 -0400

    learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(Myapp());
-
 }
 
 class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
+  //const Myapp({Key? key}) : super(key: key);
+  int question_no=0;
+  void answer()
+  {
+    print(question_no=question_no+1);
 
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var question=['What is your name','What is your height',];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+        title: Text("Quiz")
+        ),
+        body:Column(children:[
+          Text(question[question_no]),
+          ElevatedButton(
+            child:
+              Text("ans"),
+
+              onPressed: answer,
+          ),
+          ElevatedButton(onPressed: (){}, child: null,),
+          ElevatedButton(onPressed: (){},child: null,),
+
+        ],)
+      ),
+    );
   }
 }
